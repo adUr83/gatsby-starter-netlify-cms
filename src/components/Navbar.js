28e9ac0bf8462458c5cff8b86f51ffa98cd1116c
ADEM URE <58220407+adUr83@users.noreply.github.com>
2021-02-22 20:27:53 +0100

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,8 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+         Adem Tamplate
+              {/* <img src={logo} alt="Kaldi" style={{ width: '88px' }} /> */}
             </Link>
             {/* Hamburger menu */}
             <div
